61cb0d65e02b8c99a3f19d50c2b50ef7b85f8057
ev1lauronBR <110705895+ev1lauronBR@users.noreply.github.com>
2026-07-22 21:11:22 -0400

    Update Program.cs

--- backend/Program.cs
+++ backend/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+int quantidade = "teste";
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
